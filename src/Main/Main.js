61c71c0af482c84316cd4c7fe441d96c797533ea
Andrew Tran <avtran23@gmail.com>
2017-04-30 18:44:27 -0700

    [progress]Create router, app comp, main comp

---
+++ src/Main/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import './Main.css'
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/store" component={Store} />
+          <Route path="/portfolio" component={Portfolio} />
+        </Switch>
+      </main>
+    )
+  }
+}
+
+export default Main
