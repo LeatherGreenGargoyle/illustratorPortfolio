cb2d0d0d234f989a0339347210c054f6b3c7ca59
leathergreengargoyle <avtran23@gmail.com>
2017-05-07 16:05:58 -0700

    [feat]Add render tests for About, Header, Home

---
+++ src/Header/Header.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { MemoryRouter } from 'react-router-dom'
+import Header from './Header'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>,
+    div,
+  )
+})
+
+it('navigates to Home correctly')
+it('navigates to About correctly')
+it('navigates to Store correctly')
+it('navigates to Portfolio correctly')
+
+
