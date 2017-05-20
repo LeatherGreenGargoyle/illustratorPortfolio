cf994e88a7c9f00a43e0601f1a0ae505805e1650
leathergreengargoyle <avtran23@gmail.com>
2017-05-19 20:47:52 -0700

    [fix]refactor portfolio tests, prep for redux install

--- src/Portfolio/Portfolio.test.js
+++ src/Portfolio/Portfolio.test.js
@@ -2,7 +2,11 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Portfolio from './Portfolio'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
+let div
+beforeEach(() => {
+  div = document.createElement('div')
   ReactDOM.render(<Portfolio />, div)
 })
+
+it('retrieves image URLs upon mounting')
+it('contains images after rendering')
