d1153545af46ccb3f90c23c3c4460c463ec5a95d
leathergreengargoyle <avtran23@gmail.com>
2017-05-30 20:28:00 -0700

    [fix]add thunk to store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 export default rootReducer => {
   const plugins = [
     logger(),
+    thunk,
   ]
 
   const middleware = applyMiddleware(...plugins)
